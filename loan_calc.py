48f38654aa1ba16063255937fa71ee0c385f14c8
adnan77ai <yasinadnan79@gmail.com>
2022-11-30 01:25:01 +0600

    init

---
+++ loan_calc.py
@@ -0,0 +1,11 @@
+#A loan calculator that shows how much money you owe for a loan of $1,000 with a 5% APR (APR is fancy for Annual Percentage Rate) over 10 years.
+
+amount = int (1000)         #amount of loan.
+
+apr = float (0.05)          #5% annual percentage rate.
+  
+for year in range (10) :    #10 years duration.
+  
+  amount += amount*apr      #calculating loan
+
+  print (round(amount,2))
